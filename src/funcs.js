3f6ec1eb1310b4c227662a902aff6839692feaf1
Raina Jacobskind <Raina.Jacobskind@gmail.com>
2017-03-13 09:39:17 -0400

    small changes

--- src/funcs.js
+++ src/funcs.js
@@ -2,8 +2,6 @@ import recognition from './audio';
 //import App from './App';
 
 var recognizing = true;
-let count = 0; //to be used to grab every nth result to analyze
-let totalLength = 0;
 
 export const startButton = (event) => {
       recognizing = !recognizing;
@@ -26,10 +24,16 @@ recognition.onresult = (event) => {
   let isFinal = event.results[event.results.length -1].isFinal;
   if(!isFinal) { return; }
 
+
+console.log(event.results[event.results.length - 1][0].transcript);
   //length is the # of words found in a given result
   let length = event.results[event.results.length - 1][0].transcript.split(' ').length - 1;
   let message;
-  if(length < 30) { message = "You are so well spoken!"; }
+  if (length < 20) { message = "You are so well spoken!"; }
   else { message = "Slow down!"; }
   document.getElementById('message').innerHTML = message;
 }
+
+// recognition.onend = function(){
+//     recognition.start();
+// }
